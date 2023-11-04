ce5c5f5610f028dc17ca9e975e1f43c55f23b1f3
motyash <96419909+motyash@users.noreply.github.com>
2023-11-04 08:18:35 +0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,276 @@
+const contractAddress = '0xd9145CCE52D386f254917e481eB44e9943F39138'; // Замените на адрес вашего контракта
+const contractABI = [
+  [
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_stakingPeriod",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_totalRewards",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "_tokenAddress",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_user",
+				"type": "address"
+			}
+		],
+		"name": "calculateRewards",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "distributeRewards",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_user",
+				"type": "address"
+			}
+		],
+		"name": "getRewardsBalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_user",
+				"type": "address"
+			}
+		],
+		"name": "getStakedBalance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "stake",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "stakedBalances",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "stakers",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "stakingPeriod",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "stakingStartTimes",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "token",
+		"outputs": [
+			{
+				"internalType": "contract IERC20",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalRewards",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "userBalances",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "withdraw",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
+];
+
+const web3 = new Web3(window.ethereum);
+
+const contract = new web3.eth.Contract(contractABI, contractAddress);
+
+async function updateUserInfo() {
+  const userAddress = await web3.eth.getAccounts();
+  const userBalance = await web3.eth.getBalance(userAddress[0]);
+  const userStakedBalance = await contract.methods.getStakedBalance(userAddress[0]).call();
+  const userRewards = await contract.methods.getRewardsBalance(userAddress[0]).call();
+
+  document.getElementById('userAddress').textContent = userAddress[0];
+  document.getElementById('userBalance').textContent = web3.utils.fromWei(userBalance, 'ether');
+  document.getElementById('userStakedBalance').textContent = web3.utils.fromWei(userStakedBalance, 'ether');
+  document.getElementById('userRewards').textContent = web3.utils.fromWei(userRewards, 'ether');
+}
+
+async function stake() {
+  const stakeAmount = parseFloat(document.getElementById('stakeAmount').value);
+  if (stakeAmount > 0) {
+    const userAddress = await web3.eth.getAccounts();
+    await contract.methods.stake(web3.utils.toWei(stakeAmount.toString(), 'ether')).send({ from: userAddress[0] });
+    updateUserInfo();
+  }
+}
+
+async function withdraw() {
+  const userAddress = await web3.eth.getAccounts();
+  await contract.methods.withdraw().send({ from: userAddress[0] });
+  updateUserInfo();
+}
+
+window.addEventListener('load', async () => {
+  if (typeof web3 !== 'undefined') {
+    window.ethereum.enable();
+    updateUserInfo();
+  }
+});
